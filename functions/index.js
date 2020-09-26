3da62ee696850503712e0e1892905410f33ce745
Wrushasen Dakhane <wrushu2004@gmail.com>
2020-09-26 06:22:21 -0700

    Stripe Payments handling

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+const functions = require('firebase-functions');
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+//firebase functions:config:set stripe.sk=
+const stripe = require("stripe")(functions.config().stripe.sk)
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+app.listen(port, error => {
+  if (error) return error;
+  console.log("Server listening on port: ", port)
+})
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd"
+  }
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr })
+    } else {
+      res.status(200).send({ success: stripeRes })
+    }
+  })
+})
+
+exports.app = functions.https.onRequest(app)
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
